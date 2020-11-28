391b5b565476e6feb16ca1204b03bd0bc5f5896e
Heather Nuffer <zzzuzz@gmail.com>
2020-11-27 17:58:04 -0700

    Feat: Adds 3 more gratitude days

--- store/days.js
+++ store/days.js
@@ -23,5 +23,26 @@ export const state = () => ({
         'they make life at home more interesting and create opportunities for my kids to learn about caring for animals',
       image_url: 'snake.png',
     },
+    {
+      number: 'four',
+      item: 'a Thanksgiving feast',
+      image_url: 'pie.png',
+      reason:
+        "we appreciate being to eat together as a family after easily gathering everything we'd like to eat, including Trader Joe's No-Turkey Roast, riced cauliflower stuffing, and fresh green beans. Also, loving neighbors contributed a yummy pie and funeral potatoes to our meal",
+    },
+    {
+      number: 'five',
+      item: 'the outdoors',
+      reason:
+        'mountain biking, hiking, picnics, and just escaping the indoors every once in a while',
+      image_url: 'bike.png',
+    },
+    {
+      number: 'six',
+      item: 'music',
+      reason:
+        'we can dance, whistle, listen to it together, chuckle at corny or creative lyrics, and create',
+      image_url: 'music.png',
+    },
   ],
 })
