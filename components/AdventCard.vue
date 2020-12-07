60abdcbaf164a39dcf7dc49a806602640bd01c26
Heather Nuffer <zzzuzz@gmail.com>
2020-12-06 21:49:31 -0700

    Feat: Starts Christmas Advent page

---
+++ components/AdventCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div
+      v-if="!isOpen"
+      class="w-32 h-32 text-white text-xl font-bold flex justify-center items-center rounded"
+      :class="[date % 2 === 0 ? 'bg-green-600' : 'bg-red-600']"
+      @click="handleClick"
+    >
+      <span class="cursor-pointer">{{ date + 1 }}</span>
+    </div>
+    <div
+      v-else
+      class="w-32 h-32 bg-white text-xs font-bold flex flex-col justify-center items-center rounded"
+      :class="[date % 2 === 0 ? 'text-green-600' : 'text-red-600']"
+      @click="handleClick"
+    >
+      <img
+        :src="date === 24 ? 'stars.gif' : surpriseUrl"
+        alt="inside surprise"
+        class="w-24 h-24 rounded"
+      />
+      <span v-if="date !== 24" class="text-xs">{{ message }}</span>
+      <span v-else class="text-xs">Peace & Joy</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdventCard',
+  props: {
+    date: {
+      type: Number,
+      default: 1,
+    },
+    surpriseUrl: {
+      type: String,
+      default: 'cat.gif',
+    },
+    message: {
+      type: String,
+      default: 'Happy Day!',
+    },
+  },
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+  methods: {
+    handleClick() {
+      this.isOpen = !this.isOpen
+    },
+  },
+}
+</script>
---
+++ pages/advent/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container flex flex-col">
+    <h1 class="text-xl pb-6">Countdown to Christmas</h1>
+    <div class="grid grid-cols-5 gap-4">
+      <AdventCard
+        v-for="index in shuffledDates"
+        :key="index"
+        :date="index"
+        :message="index % 2 === 0 ? 'Ho ho ho' : 'Happy Day'"
+        :surprise-url="index % 2 === 0 ? 'cat2.gif' : 'cat.gif'"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AdventCard from '~/components/AdventCard'
+export default {
+  components: {
+    AdventCard,
+  },
+  computed: {
+    shuffledDates() {
+      const dates = [...Array(25).keys()]
+      // Fisher Yates Shuffle
+      for (let i = dates.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i)
+        const k = dates[i]
+        dates[i] = dates[j]
+        dates[j] = k
+      }
+      return dates
+    },
+  },
+}
+</script>
