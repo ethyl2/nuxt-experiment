aa520fe40d4922526e1af4b641473d5257b194b0
Heather Nuffer <zzzuzz@gmail.com>
2020-12-08 22:06:45 -0700

    Feat: Adds hover styles for advent days

--- components/AdventCard.vue
+++ components/AdventCard.vue
@@ -2,15 +2,20 @@
   <div>
     <div
       v-if="!isOpen"
-      class="w-32 h-32 text-white text-3xl font-bold flex justify-center items-center rounded"
-      :class="[date % 2 === 0 ? 'bg-green-600' : 'bg-red-600']"
+      class="w-32 h-32 text-white text-3xl font-bold flex justify-center items-center rounded hover:text-gray-700"
+      :class="[
+        date % 2 === 0
+          ? 'bg-green-600 hover:bg-green-400'
+          : 'bg-red-600 hover:bg-red-400',
+        date === currentDate - 1 ? 'border-4 border-yellow-400' : 'border-none',
+      ]"
       @click="handleClick"
     >
       <span class="cursor-pointer">{{ date + 1 }}</span>
     </div>
     <div
       v-else-if="isOpen"
-      class="w-32 h-32 bg-white text-xs font-bold flex flex-col justify-center items-center rounded"
+      class="w-32 h-32 bg-white text-xs font-bold flex flex-col justify-center items-center rounded hover:bg-green-200 hover:font-extrabold"
       :class="[date % 2 === 0 ? 'text-green-600' : 'text-red-600']"
       @click="handleClick"
     >
@@ -64,8 +69,6 @@ export default {
       if (this.allowedToOpen) {
         this.isOpen = !this.isOpen
       }
-      console.log(this.allowedToOpen)
-      // this.isOpen = !this.isOpen
     },
     openOldDoors() {
       if (this.date < this.currentDate - 1) {
