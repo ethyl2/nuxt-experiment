8f697f0fd14ed2827adf95c9d70814ee8184c552
Heather Nuffer <zzzuzz@gmail.com>
2021-01-06 21:17:38 -0700

    Feat: Adds quotes page

---
+++ pages/quotes/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="m-20 flex flex-col items-center space-y-4 min-h-screen">
+    <h1 class="text-3xl">Inspirational Quotes</h1>
+    <button
+      type="button"
+      class="bg-teal-700 p-1 rounded hover:bg-teal-400"
+      @click="showQuote"
+    >
+      Get Inspiration
+    </button>
+    <div
+      v-if="currentQuote"
+      class="flex flex-col items-center justify-center space-y-4"
+    >
+      <h2 class="text-xl">{{ currentQuote.text }}</h2>
+      <p v-if="currentQuote.author" class="self-end">
+        - {{ currentQuote.author }}
+      </p>
+      <img
+        :src="imgSrc"
+        alt="random image from https://picsum.photos"
+        class="rounded"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quotes',
+  async fetch() {
+    this.quotes = await fetch('https://type.fit/api/quotes').then((res) =>
+      res.json()
+    )
+  },
+  data() {
+    return {
+      quotes: [],
+      currentQuote: null,
+      imgSrc: 'https://picsum.photos/400/500',
+      imgOptions: [
+        '/300',
+        '/200/300',
+        '/200/300?grayscale',
+        '/300?grayscale',
+        '/500',
+        '/500?grayscale',
+        '/600',
+        '/600?grayscale',
+        '/400/500',
+        '/400/500?grayscale',
+      ],
+    }
+  },
+  methods: {
+    showQuote() {
+      this.currentQuote = this.quotes[
+        Math.floor(Math.random() * this.quotes.length - 1)
+      ]
+      //   const currentImgOption = this.imgOptions[
+      //     Math.floor(Math.random() * this.imgOptions.length)
+      //   ]
+      this.imgSrc = `https://picsum.photos${
+        this.imgOptions[Math.floor(Math.random() * this.imgOptions.length)]
+      }`
+    },
+  },
+}
+</script>
