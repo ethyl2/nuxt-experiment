77e847db4513947fce6908a2f35357fce65ce017
Heather Nuffer <zzzuzz@gmail.com>
2021-01-07 21:56:32 -0700

    Feat: Begins component about cats

---
+++ pages/quotes/cats.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="m-10 flex flex-col items-center space-y-4 min-h-screen text-center md:m-20"
+  >
+    <img v-if="imageUrl" :src="imageUrl" :alt="quote.text" />
+    <div class="w-1/2 mx-auto">
+      <h2 class="text-xl">{{ quote.text }}</h2>
+      <p v-if="quote.author" class="md:self-end">- {{ quote.author }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cats',
+  data() {
+    return {
+      index: null,
+      quote: null,
+    }
+  },
+  computed: {
+    quotes() {
+      return this.$store.state.cats.quotes
+    },
+    imageUrl() {
+      return this.quote.text.length < 60
+        ? `https://cataas.com/cat/cute/says/${this.quote.text}`
+        : 'https://cataas.com/cat'
+    },
+  },
+  created() {
+    this.index = Math.floor(Math.random() * this.quotes.length)
+    this.quote = this.quotes[this.index]
+  },
+}
+</script>
---
+++ store/cats.js
@@ -0,0 +1,148 @@
+export const state = () => ({
+  quotes: [
+    {
+      text:
+        'In ancient times cats were worshipped as gods; they have not forgotten this.',
+      author: 'Terry Pratchett',
+    },
+    {
+      text:
+        'I had been told that the training procedure with cats was difficult. It’s not. Mine had me trained in two days.',
+      author: 'Bill Dana',
+    },
+    {
+      text: 'Cats are inquisitive, but hate to admit it.',
+      author: 'Mason Cooley',
+    },
+    {
+      text:
+        'As anyone who has ever been around a cat for any length of time well knows, cats have enormous patience with the limitations of the humankind.',
+      author: 'Cleveland Amory',
+    },
+    {
+      text:
+        'I have studied many philosophers and many cats. The wisdom of cats is infinitely superior.I have studied many philosophers and many cats. The wisdom of cats is infinitely superior.',
+      author: 'Hippolyte Taine',
+    },
+    {
+      text:
+        'There are two means of refuge from the miseries of life: music and cats.',
+      author: 'Albert Schweitzer',
+    },
+    {
+      text:
+        'Owners of dogs will have noticed that, if you provide them with food and water and shelter and affection, they will think you are God. Whereas owners of cats are compelled to realize that, if you provide them with food and water and affection, they draw the conclusion that they are God.',
+      author: 'Christopher Hitchens',
+    },
+    {
+      text:
+        'A happy arrangement: many people prefer cats to other people, and many cats prefer people to other cats.',
+      author: 'Mason Cooley',
+    },
+    {
+      text:
+        'It is impossible for a lover of cats to banish these alert, gentle, and discriminating friends, who give us just enough of their regard and complaisance to make us hunger for more.',
+      author: 'Agnes Repplier',
+    },
+    {
+      text:
+        'How we behave toward cats here below determines our status in heaven.',
+      author: 'Robert A. Heinlein',
+    },
+    {
+      text: 'I used to love dogs until I discovered cats.',
+      author: 'Nafisa Joseph',
+    },
+    {
+      text: 'You can not look at a sleeping cat and feel tense.',
+      author: 'Jane Pauley',
+    },
+    {
+      text: 'The phrase ‘domestic cat’ is an oxymoron.',
+      author: ' George Will',
+    },
+    {
+      text: 'One cat just leads to another.',
+      author: 'Ernest Hemingway',
+    },
+    {
+      text:
+        'I have felt cats rubbing their faces against mine and touching my cheek with claws carefully sheathed. These things, to me, are expressions of love.',
+      author: 'James Herriot',
+    },
+    {
+      text: 'Kittens are angels with whiskers.',
+      author: 'Alexis Flora Hope',
+    },
+    {
+      text:
+        'A kitten is the delight of a household. All day long a comedy is played out by an incomparable actor.',
+      author: 'Champfleury',
+    },
+    {
+      text:
+        'Perhaps one reason we are fascinated by cats is because such a small animal can contain so much independence, dignity, and freedom of spirit. Unlike the dog, the cat’s personality is never bet on a human’s. He demands acceptance on his own terms.',
+      author: 'Lloyd Alexander',
+    },
+    {
+      text:
+        'Cats know how to obtain food without labor, shelter without confinement, and love without penalties.',
+      author: 'W. L. George',
+    },
+    {
+      text:
+        'Cats have it all – admiration, an endless sleep, and company only when they want it.',
+      author: 'Rod McKuen',
+    },
+    {
+      text:
+        'A cat has absolute emotional honesty: human beings, for one reason or another, may hide their feelings, but a cat does not.',
+      author: 'Ernest Hemingway',
+    },
+    {
+      text: 'Time spent with cats is never wasted.',
+      author: 'Sigmund Freud',
+    },
+    {
+      text: 'Cats rule the world.',
+      author: 'Jim Davis',
+    },
+    {
+      text: 'If cats could talk, they wouldn’t.',
+      author: 'Nan Porter',
+    },
+    {
+      text:
+        'A man who carries a cat by the tail learns something he can learn in no other way.',
+      author: 'Mark Twain',
+    },
+    {
+      text:
+        'The problem with cats is that they get the same exact look whether they see a moth or an axe-murderer.',
+      author: 'Paula Poundstone',
+    },
+    {
+      text: "People who don't like cats were probably mice in an earlier life.",
+      author: 'Anonymous',
+    },
+    {
+      text:
+        "Dogs come when they're called. Cats take a message and get back to you.",
+      author: 'Mary Bly',
+    },
+    {
+      text: 'Dogs have owners, cats have staff.',
+      author: 'Anonymous',
+    },
+    {
+      text:
+        'The mathematical probability of a common cat doing exactly as it pleases is the one scientific absolute in the world.',
+      author: 'Lynn M. Osband',
+    },
+    {
+      text:
+        "Cats have a scam going - you buy the food, they eat the food, they go away; that's the deal.",
+      author: 'Eddie Izzard',
+    },
+  ],
+})
