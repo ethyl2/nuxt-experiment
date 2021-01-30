d2b07d9f4294172dbb8f24c112f0eea84a1a7d2d
Heather Nuffer <zzzuzz@gmail.com>
2021-01-29 21:06:24 -0700

    Starts drag and drop page

---
+++ pages/arts/drag-and-drop.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="m-10">
+    <div class="drop-zone bg-teal-500 p-2">
+      <div v-for="item in listOne" :key="item.title" class="drag-el">
+        {{ item.title }}
+      </div>
+    </div>
+    <div class="drop-zone bg-red-500 p-2">
+      <div v-for="item in listTwo" :key="item.title" class="drag-el">
+        {{ item.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DragAndDrop',
+  data() {
+    return {
+      items: [
+        {
+          id: 0,
+          title: 'Aluminum',
+          list: 1,
+        },
+        {
+          id: 1,
+          title: 'Borium',
+          list: 1,
+        },
+        {
+          id: 2,
+          title: 'Calcium',
+          list: 2,
+        },
+      ],
+    }
+  },
+  computed: {
+    listOne() {
+      return this.items.filter((item) => item.list === 1)
+    },
+    listTwo() {
+      return this.items.filter((item) => item.list === 2)
+    },
+  },
+}
+</script>
