20fefe1ad6ce416609eb5f834bd910810b99f04a
Heather Nuffer <zzzuzz@gmail.com>
2021-01-30 00:15:15 -0700

    Nav: Adds link to drag and drap page to arts page

--- pages/arts/index.vue
+++ pages/arts/index.vue
@@ -8,16 +8,19 @@
       class="w-1/3 mr-0 my-4 order-2 md:order-1"
     />
     <div
-      class="flex flex-col space-y-2 order-1 md:w-1/3 md:order-2 md:space-y-6"
+      class="flex flex-col space-y-1 order-1 md:w-1/2 md:order-2 md:pl-4 md:space-y-0"
     >
       <h1 class="text-5xl">Arts</h1>
-      <nav class="flex flex-col text-lg space-y-4">
+      <nav class="flex flex-col text-lg space-y-2">
         <nuxt-link to="/canvas">Time to Draw</nuxt-link>
         <nuxt-link to="/songs">Find Songs</nuxt-link>
         <nuxt-link to="/quotes">Become Inspired</nuxt-link>
         <nuxt-link to="/quotes/cats">Celebrate Cats</nuxt-link>
         <nuxt-link to="/sorting-hat">Join a Hogwarts House</nuxt-link>
         <nuxt-link to="/sound-machine">Make a Sound Sentence</nuxt-link>
+        <nuxt-link to="/arts/drag-and-drop"
+          >Explore Your Feelings About Words</nuxt-link
+        >
       </nav>
     </div>
   </div>
