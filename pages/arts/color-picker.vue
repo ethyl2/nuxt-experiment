51fd26032dc6dd9d72cdd21cbee367292331b626
Heather Nuffer <zzzuzz@gmail.com>
2021-02-18 21:36:22 -0700

    Feat: Adds color picker page

---
+++ pages/arts/color-picker.vue
@@ -0,0 +1,95 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center text-center mx-4 md:mx-10"
+  >
+    <h1 class="text-2xl mb-4">Color Picker</h1>
+    <p class="mb-2">
+      Either enter a hex code (# with 6 digits), click on the small box, or
+      click on a pill, to select a color.
+    </p>
+    <form class="flex flex-col" @submit.prevent="getColorName">
+      <div class="flex justify-center items-stretch">
+        <input
+          v-model="hexColor"
+          type="text"
+          minlength="7"
+          maxlength="7"
+          class="text-black text-lg rounded p-1"
+        />
+        <input
+          v-model="hexColor"
+          class="rounded h-10 w-10 ml-2 outline-none"
+          type="color"
+        />
+      </div>
+      <button type="submit" class="p-1 border bg-black text-white rounded my-2">
+        Does it have a name?
+      </button>
+    </form>
+    <p v-if="nameColor" class="py-2">{{ nameColor }}</p>
+    <div
+      v-if="hexColor"
+      class="flex justify-center items-center w-20 h-20 rounded mx-auto text-xl mb-2"
+      :style="backgroundStyles"
+    >
+      😁
+    </div>
+
+    <h2 class="text-lg">Color Pills</h2>
+    <div class="flex justify-center items-center flex-wrap">
+      <div
+        v-for="color in colorNames"
+        :key="color[0]"
+        class="py-1 px-2 rounded-full m-2 hover:text-black"
+        :style="boxStyle(color[0])"
+        @click="changeColor(color)"
+      >
+        {{ color[1] }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorPicker',
+  data() {
+    return {
+      nameColor: '',
+      hexColor: '',
+    }
+  },
+  computed: {
+    colorNames() {
+      return this.$store.state.colors.colorNames
+    },
+    backgroundStyles() {
+      return {
+        'background-color': this.hexColor,
+      }
+    },
+  },
+  methods: {
+    getColorName() {
+      this.hexColor = this.hexColor.toUpperCase()
+      const colorName = this.colorNames.find(
+        (e) => e[0] === this.hexColor.slice(1)
+      )
+      if (colorName) {
+        this.nameColor = colorName[1]
+      } else {
+        this.nameColor = `Sorry, no name found for ${this.hexColor}. What would you call it?`
+      }
+    },
+    boxStyle(hexColor) {
+      return {
+        'background-color': `#${hexColor}`,
+      }
+    },
+    changeColor(color) {
+      this.hexColor = `#${color[0]}`
+      this.nameColor = color[1]
+    },
+  },
+}
+</script>
