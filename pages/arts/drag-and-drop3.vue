74ed73ea91d7f07b4e087971a228026a27bc55cd
Heather Nuffer <zzzuzz@gmail.com>
2021-04-03 13:57:38 -0600

    Adds flower arrangement example

--- pages/arts/drag-and-drop3.vue
+++ pages/arts/drag-and-drop3.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="relative min-h-screen">
     <h1 class="text-2xl text-center my-2">Make a Flower Arrangement</h1>
+    <img
+      class="mx-auto w-20 rounded"
+      src="/flowers/flowers.png"
+      alt="flower arrangement example"
+    />
     <div class="mt-4 py-2">
       <h2 class="text-xl text-center">Flower and Leaf Selection</h2>
       <p class="text-green-500 text-center mb-2">
