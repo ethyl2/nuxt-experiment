a61fbe367f46368d272b989699ec99be7c18b674
Heather Nuffer <zzzuzz@gmail.com>
2021-04-04 13:56:50 -0600

    Start podcast and css variables pages

---
+++ pages/podcast-list.vue
@@ -0,0 +1,32 @@
+<style scoped>
+ul.podcast-list li::before {
+  content: var(--bullet);
+  padding-right: 0.5em;
+}
+</style>
+<template>
+  <div class="podcast-list container flex flex-col">
+    <h1>Favorite Podcasts</h1>
+    <ul class="podcast-list">
+      <li>
+        <a href="https://www.goodjobbrain.com/" target="_blank" nofollow
+          >Good Job, Brain!</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.themcelroy.family/2018/11/2/18113030/the-empty-bowl-one"
+          target="_blank"
+          nofollow
+          >The Empty Bowl</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PodcastList',
+}
+</script>
---
+++ pages/stem/css-variables.vue
@@ -0,0 +1,72 @@
+<style scoped>
+:root {
+  --h: 50px;
+  --bullet2: '$';
+}
+.row {
+  width: 100px;
+  height: 50px;
+  background: navy;
+  position: relative;
+}
+
+.row::after {
+  content: '::after';
+  display: block;
+  position: absolute;
+  width: 100px;
+  height: 50px;
+  line-height: 50px;
+  background: gold;
+  text-align: center;
+  top: var(--h);
+}
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  min-width: 576px;
+  margin: auto;
+  padding: 20px;
+}
+
+form {
+  display: flex;
+  width: 80%;
+  justify-content: flex-start;
+}
+
+label {
+  width: 80%;
+  line-height: 40px;
+}
+
+input {
+  display: block;
+  width: 10%;
+  margin: auto;
+  color: black;
+}
+</style>
+<template>
+  <div class="wrapper">
+    <div class="row"></div>
+    <label for="y"
+      >Change the value of the <b>top</b> for <b>::after</b>
+      pseudoelement
+      <input id="y" type="number" value="0" step="10" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CssVariables',
+  mounted() {
+    const height = document.getElementById('y')
+    height.addEventListener('change', move, false)
+    function move() {
+      document.documentElement.style.setProperty('--h', height.value + 'px')
+    }
+  },
+}
+</script>
