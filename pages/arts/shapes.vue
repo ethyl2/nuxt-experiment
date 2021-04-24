475ed056fc232460d3bbaf31b8b551bf3b849c3e
Heather Nuffer <zzzuzz@gmail.com>
2021-04-24 17:40:47 -0600

    Begins css shapes page

---
+++ pages/arts/shapes.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mx-4 md:mx-20">
+    <h1>CSS Shapes</h1>
+    <div class="flex items-center justify-center w-24 h-24 bg-teal-500">
+      Square
+    </div>
+    <div class="mx-auto flex items-center justify-center w-24 h-24 bg-teal-500">
+      Square
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CssShapes',
+}
+</script>
