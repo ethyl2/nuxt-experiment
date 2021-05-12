efe40cddc6b764132ee32e94348df23f1e8b212f
Heather Nuffer <zzzuzz@gmail.com>
2021-05-11 19:40:54 -0600

    Starts stories page

---
+++ pages/inspiration/conference-stories.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container flex flex-col">
+    <h1 class="text-2xl">Favorite Stories from General Conference</h1>
+    <h2 class="text-xl">
+      A collection of stories told during
+      <a
+        href="https://www.churchofjesuschrist.org/study/general-conference?lang=eng"
+        target="_blank"
+        >general conference</a
+      >
+      sessions of
+      <a href="https://www.churchofjesuschrist.org" target="_blank"
+        >The Church of Jesus Christ of Latter-day Saints</a
+      >
+    </h2>
+    <ul>
+      <li>
+        <a
+          href="https://www.churchofjesuschrist.org/study/new-era/1973/01/the-currant-bush?lang=eng"
+          target="_blank"
+          >The Current Bush</a
+        >
+        by Elder Hugh B. Brown.
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConferenceStories',
+}
+</script>
