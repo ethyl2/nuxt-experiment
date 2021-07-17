5d547f0b60f158e4bcc46dcf50323616f5fa9b2f
Heather Nuffer <zzzuzz@gmail.com>
2021-07-17 00:50:51 -0600

    Finishes personality quiz (maybe)

--- pages/games/personality-quiz.vue
+++ pages/games/personality-quiz.vue
@@ -1,4 +1,7 @@
 <style scoped>
+input:hover + label {
+  background: rgba(0, 0, 0, 0.25);
+}
 input:focus + label {
   background: #998081;
 }
@@ -8,36 +11,47 @@ input:checked + label {
 </style>
 <template>
   <div
-    class="bg-no-repeat bg-cover bg-center min-h-screen"
-    style="background-image: url('/personality-quiz/bats.jpg')"
+    class="bg-no-repeat bg-cover bg-center min-h-screen pt-6 md:pt-12"
+    style="background-image: url('/personality-quiz/bats-in-sky.jpg')"
   >
     <header
       class="w-5/6 mx-auto my-6 font-bold text-white p-5 text-center border-2 bg-black bg-opacity-25 md:w-1/2"
     >
-      <h2 class="uppercase">Personality Quiz Time</h2>
-      <h1 class="text-xl md:text-5xl">
+      <h1 class="text-2xl md:text-5xl">
         Are you or are you not secretly a bat?
       </h1>
-      <p>This may change your life.</p>
+      <p class="italic">
+        Inspired by
+        <a
+          href="https://roses-personality-quiz.rosenuffergwc.repl.co/"
+          target="_blank"
+          >this quiz.</a
+        >
+      </p>
     </header>
     <main class="flex flex-col items-center justify-center mx-auto">
       <div
         v-for="(question, index) in questions"
+        :id="question.id"
         :key="question.instruction"
         class="mx-auto my-6 md:w-3/4"
       >
-        <h3
-          class="border-2 bg-black bg-opacity-50 md:w-1/2 p-4 text-xl font-bold text-center mx-auto mb-4"
+        <div
+          class="flex flex-col border-2 bg-black bg-opacity-50 md:w-1/2 px-2 py-1 text-xl font-bold text-center mx-4 mb-4 md:mx-auto md:text-2xl md:px-4 md:py-2"
         >
-          {{ index + 1 }}. {{ question.instruction }}
-        </h3>
-        <div class="flex w-full items-center justify-center space-x-8">
+          <h3>{{ index + 1 }}. {{ question.instruction }}</h3>
+          <p v-if="showReason" class="text-sm md:text-base">
+            {{ question.reason }}
+          </p>
+        </div>
+        <div class="flex w-full items-center justify-center space-x-8 px-2">
           <div v-for="option in question.options" :key="option.altText">
             <input
               :id="option.altText"
+              v-model="responses[question.id]"
               type="radio"
               :name="question.instruction"
-              value="option.altText"
+              :value="option.batlike"
               class="sr-only"
             />
             <label
@@ -45,13 +59,13 @@ input:checked + label {
               class="flex flex-col space-y-2 items-stretch justify-center p-4 rounded"
             >
               <div
-                class="w-20 h-20 bg-cover bg-center bg-no-repeat mx-auto rounded m-2 border-2 md:w-64 md:h-64"
+                class="w-24 h-20 bg-cover bg-center bg-no-repeat mx-auto rounded m-2 border-2 md:w-64 md:h-56"
                 :style="{
                   backgroundImage: `url(${option.imgUrl})`,
                 }"
               ></div>
               <p
-                class="border-2 bg-black bg-opacity-50 text-lg font-bold text-center mx-auto px-2 py-1"
+                class="border-2 bg-black bg-opacity-50 text-lg font-bold text-center mx-auto px-2 py-1 rounded md:text-xl"
               >
                 {{ option.altText }}
               </p>
@@ -59,19 +73,71 @@ input:checked + label {
           </div>
         </div>
       </div>
+
+      <button
+        v-if="
+          Object.keys(responses).length === questions.length &&
+          finalText.length === 0
+        "
+        class="text-3xl md:text-5xl bg-black py-2 px-3 rounded hover:bg-white hover:text-black my-4"
+        @click="handleSubmit"
+      >
+        Find out
+      </button>
+      <p v-else class="text-black">
+        You have made {{ Object.keys(responses).length }}/{{ questions.length }}
+        selections.
+      </p>
+      <p class="text-2xl my-6 text-black md:text-6xl">{{ finalText }}</p>
+      <img
+        v-if="finalText === 'You are.'"
+        src="/personality-quiz/bat-faces.jpg"
+        alt="bat faces"
+        class="w-32 h-30 md:w-64 md:h-56 rounded"
+      />
+      <button
+        v-if="Object.keys(responses).length === questions.length"
+        class="text-xl md:text-2xl bg-black py-2 px-3 rounded hover:bg-white hover:text-black my-4"
+        @click="reset"
+      >
+        Reset
+      </button>
     </main>
   </div>
 </template>
 <script>
 export default {
-  name: 'PodcastList',
+  name: 'PersonalityQuiz',
   data() {
     return {
+      finalText: '',
+      responses: {},
+      showReason: false,
       questions: [
         {
+          id: 'color',
+          instruction: 'Pick a color.',
+          reason:
+            'Of the 1400+ species of bats in the world, 3 are vampire bats that drink blood.',
+          reference: 'https://www.batcon.org/about-bats/faq/',
+          options: [
+            {
+              imgUrl: '/personality-quiz/green.jpg',
+              altText: 'green',
+              batlike: false,
+            },
+            {
+              imgUrl: '/personality-quiz/red.jpg',
+              altText: 'red',
+              batlike: true,
+            },
+          ],
+        },
+        {
+          id: 'character',
           instruction: 'Pick a character.',
           reason:
-            'There is a breed of fruit bat that had the nickname Yoda Bat.',
+            'There is a breed of fruit bat that had the nickname Yoda bat.',
           reference: 'https://www.livescience.com/8735-adorable-bat.html',
           options: [
             {
@@ -87,13 +153,14 @@ export default {
           ],
         },
         {
+          id: 'baby',
           instruction: 'Pick a baby animal.',
           reason: 'Baby bats are called pups, not kittens.',
           reference: 'https://www.doi.gov/blog/13-facts-about-bats',
           options: [
             {
               imgUrl: '/personality-quiz/puppy.jpg',
-              altText: 'Puppy',
+              altText: 'puppy',
               batlike: true,
             },
             {
@@ -104,6 +171,7 @@ export default {
           ],
         },
         {
+          id: 'hands',
           instruction: 'Pick a pair of hands.',
           reason:
             'Bats clean themselves. They spend a lot of time grooming themselves, and some even groom each other.',
@@ -121,8 +189,47 @@ export default {
             },
           ],
         },
+        {
+          id: 'pumpkins',
+          instruction: 'Pick a pair of pumpkins.',
+          reason:
+            'Hanging upside down allows bats to let go and quickly fall to gain momentum for flight. Unlike birds that have hollow bones, bats have solid bones like all other mammals, which is why they have more need for the extra momentum gained from falling. ',
+          reference: 'https://www.batcon.org/about-bats/faq/',
+          options: [
+            {
+              imgUrl: '/personality-quiz/painted-pumpkins.jpg',
+              altText: 'painted pumpkins',
+              batlike: true,
+            },
+            {
+              imgUrl: '/personality-quiz/jack-o-lanterns.jpg',
+              altText: 'jack-o-lanterns',
+              batlike: false,
+            },
+          ],
+        },
       ],
     }
+  },
+  methods: {
+    handleSubmit() {
+      this.finalText = 'Calculating...'
+      setTimeout(() => {
+        const trueCount = Object.values(this.responses).filter(Boolean).length
+        if (trueCount >= this.questions.length / 2) {
+          this.finalText = 'You are.'
+        } else {
+          this.finalText = 'You are not. Sorry.'
+        }
+        this.showReason = true
+      }, 1000)
+    },
+    reset() {
+      this.responses = {}
+      location.href = `#${this.questions[0].id}`
+      this.finalText = ''
+      this.showReason = false
+    },
   },
 }
 </script>
