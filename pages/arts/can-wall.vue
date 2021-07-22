37a0619137baaa28b8bb037e94f50ba50b8e42d1
Heather Nuffer <zzzuzz@gmail.com>
2021-07-21 22:35:26 -0600

    Starts can wall page

---
+++ pages/arts/can-wall.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="min-h-screen">
+    <header
+      class="h-64 bg-cover bg-no-repeat bg-center flex items-center justify-center"
+      style="background-image: url(/can-wall/can-wall.jpg)"
+    >
+      <h1
+        class="text-6xl font-bold font-black text-white text-center px-3 py-1 bg-black rounded bg-opacity-75"
+      >
+        Can Wall!
+      </h1>
+    </header>
+    <main class="mx-4 md:mx-20">
+      <p class="py-6 text-base md:text-xl">
+        My family and I have an ongoing art installation at our house -- a can
+        wall that is gradually getting bigger and bigger. It serves as a divider
+        between my workspace and my daughter's workspace, and doubles as a good
+        zoom background.
+      </p>
+      <h2 class="pb-4 text-2xl">You can make one, too!</h2>
+      <h3 class="pb-6 italic text-lg">Here's what you'll need:</h3>
+      <div
+        class="flex flex-col items-start w-full md:flex-row md:justify-between md:space-x-4"
+      >
+        <div
+          class="flex flex-col items-center justify-center w-full md:w-1/2 space-y-2"
+        >
+          <img
+            src="/can-wall/liquid-nails.jpg"
+            alt="Liquid nails glue"
+            class="w-full"
+          />
+          <p class="text-xl font-bold">1. Liquid Nails glue</p>
+          <a
+            href="https://www.amazon.com/dp/B01M5GBLYK/ref=cm_sw_r_tw_dp_0C51BND5RPZ9S4S96Y77"
+            target="_blank"
+            >Liquid Nails 5 oz. Fuze It</a
+          >
+          <p>or</p>
+          <a
+            href="https://www.amazon.com/dp/B01MFEUU45/ref=cm_sw_r_tw_dp_3NS4DA0RVR9S61F3EWTN"
+            target="_blank"
+            >Liquid Nails 9 oz. Fuze It</a
+          >
+          <img
+            src="/can-wall/caulk-gun.jpg"
+            alt="caulk-gun"
+            class="mx-auto w-2/3 md:w-1/2"
+          />
+          <p class="text-xl font-bold">
+            2. A caulk gun -- only if you use the 9 oz. glue.
+          </p>
+          <a
+            href="https://www.amazon.com/dp/B07CVSQJHY/ref=cm_sw_r_tw_dp_EAGSWRTKYTG75YZ91BYN"
+            target="_blank"
+            >A caulk gun</a
+          >
+          <p>
+            At first, we used hot glue with a hot glue gun, but some brands of
+            hot glue sticks didn't work very well. Liquid nails has been a
+            better choice.
+          </p>
+
+          <img
+            src="/can-wall/pinto-beans.jpg"
+            alt="pinto beans"
+            class="w-2/3 mx-auto md:w-1/2"
+          />
+          <p class="text-xl font-bold">3. Dry beans</p>
+          <p>
+            Any kind will do; just make sure they are dry. They will help lower
+            the center of gravity of the call wall and make it more sturdy.
+          </p>
+          <a
+            href="https://www.amazon.com/dp/B07BBW7PYV/ref=cm_sw_r_tw_dp_X2136VQZ45SR9EJARTAT"
+            target="_blank"
+            >Pinto beans</a
+          >
+
+          <img
+            src="/can-wall/duct-tape.jpg"
+            alt="duct tape"
+            class="w-2/3 mx-auto md:w-1/3"
+          />
+          <p class="text-xl font-bold">4. A bit of duct tape</p>
+          <p>
+            We don't use this for sticking cans together, but for sealing in the
+            beans.
+          </p>
+          <a
+            href="https://www.amazon.com/dp/B0000DH4ME/ref=cm_sw_r_tw_dp_4G3TH4FSGJJQS1S51SWV"
+            target="_blank"
+            >Duck Tape</a
+          >
+
+          <img
+            src="/can-wall/funnels.jpg"
+            alt="funnels"
+            class="mx-auto w-2/3 md:w-1/2"
+          />
+          <p class="text-xl font-bold">5. A funnel</p>
+          <a
+            href="https://www.amazon.com/dp/B08PK8RYGM/ref=cm_sw_r_tw_dp_1836QD54BEYYGD4QYWA1"
+            target="_blank"
+            >Collapsible funnels</a
+          >
+
+          <img
+            src="/can-wall/gloves.jpg"
+            alt="gloves"
+            class="mx-auto w-2/3 md:w-1/2"
+          />
+          <p class="text-xl font-bold">6. Gloves</p>
+          <p>Keep glue off your hands!</p>
+          <a
+            href="https://www.amazon.com/dp/B00GS8W3T4/ref=cm_sw_r_tw_dp_HBX1Y94RYC1WEDX49HJK"
+            target="_blank"
+            >Nitrile gloves</a
+          >
+          <hr
+            style="
+              height: 2px;
+              border-width: 0;
+              color: gray;
+              background-color: gray;
+            "
+          />
+        </div>
+        <div class="flex flex-col items-center justify-center w-full md:w-1/2">
+          <img src="/can-wall/cans.jpg" alt="soda cans" class="w-full" />
+          <p class="text-xl font-bold py-2">
+            7. A bunch of 12 oz. soda pop/juice/flavored water cans
+          </p>
+          <p class="pb-2">
+            We like using a variety! Here are some colorful options:
+          </p>
+          <ul>
+            <li>
+              <a
+                href="https://www.amazon.com/dp/B08NTN6Y1F/ref=cm_sw_r_tw_dp_6F32ABBKKF992BC8RACV?_encoding=UTF8&psc=1v"
+                target="_blank"
+                >Bubly</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://www.amazon.com/dp/B07FCQKZ4F/ref=cm_sw_r_tw_dp_HDKDW3KE04T0XNJQKTAF"
+                target="_blank"
+                >Spindrift</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://www.amazon.com/dp/B07TLYD6SF/ref=cm_sw_r_tw_dp_8MCJRW35Y42EFWH11ZQA"
+                target="_blank"
+                >La Croix</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://www.amazon.com/dp/B0859LT36D/ref=cm_sw_r_tw_dp_WQYGP04R3FV8RKM3QF7K"
+                target="_blank"
+                >Variety of Sodas</a
+              >
+            </li>
+          </ul>
+          <div class="flex flex-col w-full mt-6">
+            <div
+              class="bg-center bg-cover bg-no-repeat w-full h-64"
+              style="background-image: url(/can-wall/bubly-cans.jpg)"
+            ></div>
+            <div
+              class="bg-center bg-cover bg-no-repeat w-full h-64"
+              style="background-image: url(/can-wall/spindrift-cans.jpg)"
+            ></div>
+            <div
+              class="bg-center bg-cover bg-no-repeat w-full h-64"
+              style="background-image: url(/can-wall/la-croix-cans.jpg)"
+            ></div>
+            <div
+              class="bg-center bg-cover bg-no-repeat w-full h-64"
+              style="background-image: url(/can-wall/soda-cans.jpg)"
+            ></div>
+          </div>
+        </div>
+      </div>
+      <section class="mx-4 pb-4 md:mx-20 md:pb-12">
+        <h2 class="text-2xl py-4">Instructions</h2>
+        <ol
+          class="list-decimal list-inside space-y-2 p-4 rounded bg-black text-white"
+        >
+          <li>
+            Fill an empty can with dry beans. A funnel can come in handy for
+            this.
+          </li>
+          <li>Cover the opening in the can with a piece of duct tape.</li>
+          <li>
+            Continue to make bean cans for each can you want on the bottom row
+            of your can wall.
+          </li>
+          <li>Put on gloves.</li>
+          <li>
+            Glue them together, probably using about 3 dabs of glue in a
+            vertical line on each can.
+          </li>
+          <li>
+            Add a row of empty cans on top of that row, using glue on the bottom
+            of the cans, and 3 dabs of glue on the sides.
+          </li>
+          <li>
+            Adjust the cans to make sure they are as straight as possible.
+          </li>
+          <li>Let the glue dry for several hours.</li>
+          <li>Continue adding cans in stages.</li>
+        </ol>
+      </section>
+      <section
+        class="flex flex-col items-center justify-center space-y-2 mx-4 md:mx-64"
+      >
+        <img
+          src="/can-wall/rose-with-can-wall.jpg"
+          alt="Beginnings of can wall"
+        />
+        <p>Starting our can wall</p>
+        <img
+          src="/can-wall/heather-with-can-wall.jpg"
+          alt="Selfie with can wall"
+        />
+        <p>Look, I'm on zoom!</p>
+        <img src="/can-wall/can-wall-2.jpg" alt="Lots of cans" />
+        <p>The can wall is growing</p>
+        <img src="/can-wall/can-wall.jpg" alt="can wall" />
+        <p>Would this make a good jigsaw puzzle?</p>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CanWall',
+}
+</script>
