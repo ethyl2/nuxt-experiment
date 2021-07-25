426f54099a6a1a25fe4455acd4974352b99fdad2
Heather Nuffer <zzzuzz@gmail.com>
2021-07-24 23:11:32 -0600

    Adds printables page and pdf

---
+++ pages/inspiration/printables.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container min-h-screen flex flex-col items-center justify-start">
+    <h1 class="font-3xl py-4 font-bold">Printables</h1>
+    <button
+      class="bg-black text-yellow-400 font-bold py-2 px-3 w-1/2 rounded mx-auto hover:text-black hover:bg-white text-base md:text-lg md:w-auto"
+    >
+      <a href="/hebrews_11_coloring.pdf" download=""
+        >🖨️ Download Hebrews 11 Coloring Page Printable</a
+      >
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Printables',
+}
+</script>
