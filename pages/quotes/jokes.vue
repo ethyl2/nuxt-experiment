15b74dcd7f30499479a5d5331999ece0fc1d0735
Heather Nuffer <zzzuzz@gmail.com>
2021-07-30 22:28:34 -0600

    Moves jokes data into the store and adds more jokes

--- pages/quotes/jokes.vue
+++ pages/quotes/jokes.vue
@@ -26,86 +26,14 @@ export default {
   components: {
     Joke: () => import('~/components/Joke'),
   },
-  data() {
-    return {
-      jokes: [
-        {
-          question: "What do you call a snake that's shed its skin?",
-          answer: 'Naked.',
-        },
-        {
-          question: 'What do you call Santa at the South Pole?',
-          answer: 'A lost Claus.',
-        },
-        {
-          question: 'What side of a cat has the most fur?',
-          answer: 'The outside.',
-        },
-        {
-          question:
-            'What did the buffalo say when he dropped his son off at school?',
-          answer: 'Bye son.',
-        },
-        {
-          question: 'Why was the pig in the kitchen?',
-          answer: 'It was bacon.',
-        },
-        {
-          question: "Why aren't koalas actual bears?",
-          answer: "They don't meet the koalafications.",
-        },
-        {
-          question: "What's smarter than a talking parrot?",
-          answer: 'A spelling bee.',
-        },
-        {
-          question: 'How do porcupines talk to each other?',
-          answer: 'With spine language.',
-        },
-        {
-          question:
-            'What do you call 2 monkeys that share an Amazon prime account?',
-          answer: 'Prime mates.',
-        },
-        {
-          question: 'What are bears without bees?',
-          answer: 'Ears.',
-        },
-        {
-          question: "Why can't you give Elsa a balloon?",
-          answer: 'Because she will let it go...',
-        },
-        {
-          question: 'Why did half a chicken cross the road?',
-          answer: 'To get to his other side.',
-        },
-        {
-          question: 'Why did the skunk cross the road?',
-          answer: 'To get to the odor side.',
-        },
-        {
-          question: 'What kind of bagel can fly?',
-          answer: 'A plain bagel!',
-        },
-        {
-          question: 'What do you call a fake noodle?',
-          answer: 'An impasta.',
-        },
-        {
-          question:
-            'How is the alphabet different on Christmas than any other day?',
-          answer: 'On Christmas, it has Noel.',
-        },
-        {
-          question: 'Why did the couple get hitched on the 24th of December?',
-          answer: 'So they could have a married Christmas.',
-        },
-        {
-          question: 'What does the gingerbread man use to make his bed?',
-          answer: 'Cookie sheets.',
-        },
-      ],
-    }
+  computed: {
+    jokes() {
+      const jokesFromStoreCopy = JSON.parse(
+        JSON.stringify(this.$store.state.jokes.all)
+      )
+      jokesFromStoreCopy.sort(() => 0.5 - Math.random())
+      return jokesFromStoreCopy
+    },
   },
   head() {
     return {
---
+++ store/jokes.js
@@ -0,0 +1,172 @@
+export const state = () => ({
+  all: [
+    {
+      question: "What do you call a snake that's shed its skin?",
+      answer: 'Naked.',
+    },
+    {
+      question: 'What do you call Santa at the South Pole?',
+      answer: 'A lost Claus.',
+    },
+    {
+      question: 'What side of a cat has the most fur?',
+      answer: 'The outside.',
+    },
+    {
+      question:
+        'What did the buffalo say when he dropped his son off at school?',
+      answer: 'Bye son.',
+    },
+    {
+      question: 'Why was the pig in the kitchen?',
+      answer: 'It was bacon.',
+    },
+    {
+      question: "Why aren't koalas actual bears?",
+      answer: "They don't meet the koalafications.",
+    },
+    {
+      question: "What's smarter than a talking parrot?",
+      answer: 'A spelling bee.',
+    },
+    {
+      question: 'How do porcupines talk to each other?',
+      answer: 'With spine language.',
+    },
+    {
+      question:
+        'What do you call 2 monkeys that share an Amazon prime account?',
+      answer: 'Prime mates.',
+    },
+    {
+      question: 'What are bears without bees?',
+      answer: 'Ears.',
+    },
+    {
+      question: "Why can't you give Elsa a balloon?",
+      answer: 'Because she will let it go...',
+    },
+    {
+      question: 'Why did half a chicken cross the road?',
+      answer: 'To get to his other side.',
+    },
+    {
+      question: 'Why did the skunk cross the road?',
+      answer: 'To get to the odor side.',
+    },
+    {
+      question: 'What kind of bagel can fly?',
+      answer: 'A plain bagel!',
+    },
+    {
+      question: 'What do you call a fake noodle?',
+      answer: 'An impasta.',
+    },
+    {
+      question:
+        'How is the alphabet different on Christmas than any other day?',
+      answer: 'On Christmas, it has Noel.',
+    },
+    {
+      question: 'Why did the couple get hitched on the 24th of December?',
+      answer: 'So they could have a married Christmas.',
+    },
+    {
+      question: 'What does the gingerbread man use to make his bed?',
+      answer: 'Cookie sheets.',
+    },
+    {
+      question: 'What do you call an elf who has just won the lottery?',
+      answer: 'Welfy.',
+    },
+    {
+      question: 'Who says "Oh Oh Oh"?',
+      answer: 'Santa Claus walking backwards.',
+    },
+    {
+      question: 'Who delivers presents to baby sharks at Christmas?',
+      answer: 'Santa Jaws.',
+    },
+    {
+      question: 'What do you call a blind reindeer?',
+      answer: 'I have no eye deer.',
+    },
+    {
+      question: 'What do you call a scary-looking reindeer?',
+      answer: 'A cariboo.',
+    },
+    {
+      question: "What's the smartest type of animal at the North Pole?",
+      answer: 'Brain deer.',
+    },
+    {
+      question: 'Where do reindeer like to stop for ice cream?',
+      answer: 'Deery Queen.',
+    },
+    {
+      question: 'What do snowmen eat for breakfast?',
+      answer: 'Frosted Flakes or Ice Crispies.',
+    },
+    {
+      question: 'What do reindeer eat for breakfast?',
+      answer: 'Deerios.',
+    },
+    {
+      question: 'What do you call a snowman that can walk?',
+      answer: 'A snow mobile.',
+    },
+    {
+      question: "What is a Christmas tree's favorite candy?",
+      answer: 'Ornamints.',
+    },
+    {
+      question: 'Why was the snowman rummaging in a bag of carrots?',
+      answer: 'He was picking his nose.',
+    },
+    {
+      question: 'Why is everyone so thirsty at the North Pole?',
+      answer: 'No well, no well!',
+    },
+    {
+      question: 'How does Good King Wenceslas like his pizza?',
+      answer: 'Deep pan, crisp and even!',
+    },
+    {
+      question: 'What do you get if you eat onions on your beans?',
+      answer: 'Tear gas.',
+    },
+    {
+      question: 'How much do I love tacos?',
+      answer: 'From my head tomatoes.',
+    },
+    {
+      question: 'What is a perfect name for a princess who loves tacos?',
+      answer: 'Taco Bella.',
+    },
+    {
+      question: 'Who is an ideal match for the Taco Beast?',
+      answer: 'Taco Belle.',
+    },
+    {
+      question: 'How can a taco get out of taco jail?',
+      answer: 'By getting Taco Bail.',
+    },
+    {
+      question: 'Why did the waiter give me a taco when I asked for nachos?',
+      answer: 'It was just a bad queso miscommunication.',
+    },
+    {
+      question: 'What do you call a tiny boat made out of fried corn?',
+      answer: 'A tortilla ship.',
+    },
+    {
+      question: 'How does a tortilla chip sneeze?',
+      answer: '"Na-choooooo!"',
+    },
+    {
+      question:
+        ' What did one taco say to the other when he was acting unusual? ',
+      answer: '"You are nachoself today."',
+    },
+  ],
+})
