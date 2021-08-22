24fac427519593cd350201ed0b9ed5ba2c27f397
Heather Nuffer <zzzuzz@gmail.com>
2021-08-21 20:26:49 -0600

    Now user can toggle peg placement

--- pages/arts/lite-brite.vue
+++ pages/arts/lite-brite.vue
@@ -2,6 +2,10 @@
   <div class="bg-black">
     <div class="flex flex-col items-center justify-center space-y-2 py-4">
       <h1 class="text-white text-lg font-bold">Lite Brite on Your Computer!</h1>
+      <h2 class="text-white text-base">
+        Click on a 'hole' once to place a 'peg'; click again to take the 'peg'
+        out.
+      </h2>
       <div class="flex items-start justify-center space-x-4 divide-x">
         <div class="flex flex-col items-center justify-center">
           <label for="favcolor">Select your color:</label>
@@ -40,17 +44,17 @@
             <button
               class="w-5 h-5 rounded"
               style="background: #ff5f1f"
-              @click="currentColor = '#FF5F1F'"
+              @click="currentColor = '#ff5f1f'"
             ></button>
             <button
               class="w-5 h-5 rounded"
               style="background: #fff04d"
-              @click="currentColor = '#FFF04D'"
+              @click="currentColor = '#fff04d'"
             ></button>
             <button
               class="w-5 h-5 rounded"
               style="background: #0d4eff"
-              @click="currentColor = '#0D4EFF'"
+              @click="currentColor = '#0d4eff'"
             ></button>
           </div>
         </div>
@@ -76,7 +80,7 @@
         <div
           v-for="secondIndex in 75"
           :key="secondIndex"
-          class="hole border border-gray-200 border-opacity-25 rounded-full w-4 h-4 m-px flex-shrink-0 cursor-pointer"
+          class="hole border border-gray-200 border-opacity-25 rounded-full w-4 h-4 m-px flex-shrink-0 cursor-pointer bg-transparent"
           @click="handleClick"
         ></div>
       </div>
@@ -94,8 +98,18 @@ export default {
   },
   methods: {
     handleClick(event) {
-      event.target.style.background = this.currentColor
-      event.target.style.boxShadow = `0px 0px 10px 5px ${this.currentColor}`
+      let bgColor = event.target.style.background
+      const bgColorArray = bgColor.replace(/[^\d,]/g, '').split(',')
+      if (bgColorArray.length > 1) {
+        bgColor = this.rgbToHex(bgColorArray)
+      }
+      if (bgColor !== this.currentColor) {
+        event.target.style.background = this.currentColor
+        event.target.style.boxShadow = `0px 0px 10px 5px ${this.currentColor}`
+      } else {
+        event.target.style.background = 'transparent'
+        event.target.style.boxShadow = `none`
+      }
     },
     clearScreen() {
       const holes = document.querySelectorAll('.hole')
@@ -103,6 +117,18 @@ export default {
         hole.style.background = 'transparent'
         hole.style.boxShadow = 'none'
       })
+    },
+    componentToHex(c) {
+      const hex = Number(c).toString(16)
+      return hex.length === 1 ? '0' + hex : hex
+    },
+    rgbToHex(colorArray) {
+      return (
+        '#' +
+        this.componentToHex(colorArray[0]) +
+        this.componentToHex(colorArray[1]) +
+        this.componentToHex(colorArray[2])
+      )
     },
   },
 }
