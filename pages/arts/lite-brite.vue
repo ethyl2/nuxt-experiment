f6e7c496bccc6d3a548148d4c0a02d6fb61d0781
Heather Nuffer <zzzuzz@gmail.com>
2021-08-21 21:13:03 -0600

    Refactor: Now loops through color list to display buttons, in lite brite page

--- pages/arts/lite-brite.vue
+++ pages/arts/lite-brite.vue
@@ -2,13 +2,17 @@
   <div class="bg-black">
     <div class="flex flex-col items-center justify-center space-y-2 py-4">
       <h1 class="text-white text-lg font-bold">Lite Brite on Your Computer!</h1>
-      <h2 class="text-white text-base">
-        Click on a 'hole' once to place a 'peg'; click again to take the 'peg'
-        out.
+      <h2 class="text-white text-sm text-center text-base">
+        <span class="hidden md:inline">Click</span
+        ><span class="md:hidden">Tap</span> on a 'hole' once to place a 'peg';
+        <span class="hidden md:inline">click</span
+        ><span class="md:hidden">tap</span> again to take the 'peg' out.
       </h2>
-      <div class="flex items-start justify-center space-x-4 divide-x">
+      <div class="flex items-stretch justify-center space-x-4 divide-x">
         <div class="flex flex-col items-center justify-center">
-          <label for="favcolor">Select your color:</label>
+          <label for="favcolor" class="text-center text-sm md:text-base"
+            >Select your color:</label
+          >
           <input
             id="favcolor"
             v-model="currentColor"
@@ -16,52 +20,30 @@
             name="favcolor"
             class="rounded"
           />
-          <p class="text-white">{{ currentColor }}</p>
+          <p class="text-white text-sm md:text-base text-center">
+            {{ currentColor }}
+          </p>
         </div>
         <div class="px-4">
-          <p>Or use one of these colors:</p>
-          <div class="flex items-center justify-center space-x-2">
+          <p class="text-center text-sm pb-2 md:text-base">
+            Or use one of these:
+          </p>
+          <div
+            class="grid grid-cols-4 gap-y-1 mx-auto md:flex md:items-center md:justify-center md:space-x-2 md:gap-y-0"
+          >
             <button
-              class="w-5 h-5 rounded"
-              style="background: #07f2e3"
-              @click="currentColor = '#07f2e3'"
-            ></button>
-            <button
-              class="w-5 h-5 rounded"
-              style="background: #fc1c03"
-              @click="currentColor = '#fc1c03'"
-            ></button>
-            <button
-              class="w-5 h-5 rounded"
-              style="background: #3ff91a"
-              @click="currentColor = '#3ff91a'"
-            ></button>
-            <button
-              class="w-5 h-5 rounded"
-              style="background: #f90bdd"
-              @click="currentColor = '#f90bdd'"
-            ></button>
-            <button
-              class="w-5 h-5 rounded"
-              style="background: #ff5f1f"
-              @click="currentColor = '#ff5f1f'"
-            ></button>
-            <button
-              class="w-5 h-5 rounded"
-              style="background: #fff04d"
-              @click="currentColor = '#fff04d'"
-            ></button>
-            <button
-              class="w-5 h-5 rounded"
-              style="background: #0d4eff"
-              @click="currentColor = '#0d4eff'"
+              v-for="color in colors"
+              :key="color"
+              class="w-6 h-6 rounded mx-auto transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 md:w-5 md:h-5"
+              :style="{ background: `${color}` }"
+              @click="currentColor = `${color}`"
             ></button>
           </div>
         </div>
-        <div class="pl-4">
+        <div class="pl-4 flex items-center justify-center">
           <button
             type="button"
-            class="bg-gray-900 rounded px-2 py-1 hover:bg-gray-700"
+            class="bg-gray-900 rounded px-2 py-1 text-sm md:text-base hover:bg-gray-700"
             @click="clearScreen"
           >
             Clear All Pegs
@@ -70,7 +52,7 @@
       </div>
     </div>
     <div
-      class="w-full min-h-screen bg-black mx-auto overflow-auto flex-shrink-0"
+      class="w-full min-h-screen bg-black mx-auto overflow-auto flex-shrink-0 p-4"
     >
       <div
         v-for="index in 35"
@@ -81,9 +63,14 @@
           v-for="secondIndex in 75"
           :key="secondIndex"
           class="hole border border-gray-200 border-opacity-25 rounded-full w-4 h-4 m-px flex-shrink-0 cursor-pointer bg-transparent"
+          tabindex="0"
           @click="handleClick"
+          @keyup.enter="handleClick"
         ></div>
       </div>
+      <p class="text-center text-white pt-2">
+        Number of pegs placed: {{ countPegsPlaced }}
+      </p>
     </div>
   </div>
 </template>
@@ -94,6 +81,17 @@ export default {
   data() {
     return {
       currentColor: '#d228b6',
+      countPegsPlaced: 0,
+      colors: [
+        '#fc1c03',
+        '#07f2e3',
+        '#ffffff',
+        '#3ff91a',
+        '#f90bdd',
+        '#ff5f1f',
+        '#fff04d',
+        '#0d4eff',
+      ],
     }
   },
   methods: {
@@ -104,11 +102,15 @@ export default {
         bgColor = this.rgbToHex(bgColorArray)
       }
       if (bgColor !== this.currentColor) {
+        if (!bgColor) {
+          this.countPegsPlaced++
+        }
         event.target.style.background = this.currentColor
         event.target.style.boxShadow = `0px 0px 10px 5px ${this.currentColor}`
       } else {
         event.target.style.background = 'transparent'
         event.target.style.boxShadow = `none`
+        this.countPegsPlaced--
       }
     },
     clearScreen() {
